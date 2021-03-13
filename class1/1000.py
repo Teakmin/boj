dd95c784667a9981c6f563bad6f620f630be73cc
Teakmin <tmin4889@naver.com>
2021-03-13 17:14:24 +0900

    Add existing file"

--- class1/1000.py
+++ class1/1000.py
@@ -1,2 +1,29 @@
-A, B = map(int, input().split())
-print(A + B)
+C:\Users\taekm\OneDrive\바탕 화면\baekjoon>git add .
+
+C:\Users\taekm\OneDrive\바탕 화면\baekjoon>git status
+On branch master
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        new file:   class1/1000.py
+
+
+C:\Users\taekm\OneDrive\바탕 화면\baekjoon>git commit -m "Update 1000"
+[master 9f37923] Update 1000
+ 1 file changed, 2 insertions(+)
+ create mode 100644 class1/1000.py
+
+C:\Users\taekm\OneDrive\바탕 화면\baekjoon>git push origin master
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (4/4), 393 bytes | 56.00 KiB/s, done.
+Total 4 (delta 0), reused 0 (delta 0), pack-reused 0
+To https://github.com/Teakmin/boj
+   7d7a289..9f37923  master -> master
+
+
+C:\Users\taekm\OneDrive\바탕 화면\baekjoon
+
+
+
