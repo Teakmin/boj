bf1abe440ea0ff9c59bbfb9b3947632ceb2125c7
Teakmin <tmin4889@naver.com>
2021-03-17 21:33:24 +0900

    Update 1157

--- class1/시간초과 1157.py
+++ class1/시간초과 1157.py
@@ -1,21 +1,10 @@
 S = input()
-count = 0
 S = S.upper()
-S.replace(" ", "")
-list = list(S)
-for i in range(len(list)):
-    fre = list.count(list[i])
-    if fre > count:
-        count = fre
-        res = list[i]
-        notOne = 0
-    elif fre == count:
-        if res == list[i]:
-            pass
-        else:
-            notOne += 1
-
-if notOne == 0:
-    print(res)
-else:
+alpha = [0] * 26
+for i in S:
+    alpha[ord(i)-65] += 1
+max=max(alpha)
+if alpha.count(max) > 1:
     print("?")
+else:
+    print(chr(alpha.index(max) + 65))
