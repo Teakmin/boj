3738008a819cc7dd8337a0202e6e9d83d15a732c
Teakmin <tmin4889@naver.com>
2021-03-29 20:29:09 +0900

    Update 10869.py, 10871.py, 10950.py, 10952.py, 11654.py, 11720.py

---
+++ class1/11720.py
@@ -0,0 +1,6 @@
+T = int(input())
+A = str(input())
+sum = 0
+for i in range(T):
+    sum += int(A[i])
+print(sum)
---
+++ class1/Qu10951.py
@@ -0,0 +1,4 @@
+# runtime error가 계속 뜸. while True가 문제인가?
+while True:
+    A, B = map(int, input().split())
+    print(A + B)
