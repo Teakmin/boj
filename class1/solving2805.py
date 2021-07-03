828182bb592d6876bf8b1104fd953193baa74ef5
Teakmin <tmin4889@naver.com>
2021-07-03 23:52:17 +0900

    2021-07-03

--- class1/solving2805.py
+++ class1/solving2805.py
@@ -1,20 +1,29 @@
- N = int(input())
- M = int(input())
- arr = list(map(int,input().split()))
- arr = sorted(arr)
- min = min(arr)
- max = max(arr)
+#시간초과 (googling하고 고친건데도 시간초과)
+import sys
+input = sys.stdin.readline
 
- mid = (max + min)//2
- sum = 0
- while result != 1:
-     for i in range(N):
-         if arr[i] > mid:
-             sum += arr[i] - mid
-             if i == N-1:
-                 if sum == M:
-                     result = 1    #while문  문제가 생김
-                 else:
-                     result = 0
-    if sum < M:
-        mid = (arr[])
+N, M = map(int, input().split())
+trees = list(map(int, input().split()))
+start = 9
+end = max(trees)
+maxcut = 0
+
+while start <= end:
+    mid = (start + end)//2
+    sum = 0
+
+    for tree in trees:
+        if tree-mid < 0:
+            continue
+        sum += tree-mid
+
+    if sum == M:
+        print(mid)
+        sys.exit()
+    elif sum > M:
+        if maxcut < mid:
+            maxcut = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+print(maxcut)
