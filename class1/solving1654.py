482276a02df8a6ad0d0a4a48b5107e34c830fc35
Teakmin <tmin4889@naver.com>
2021-07-10 00:26:49 +0900

    2021-07-10

--- class1/solving1654.py
+++ class1/solving1654.py
@@ -1,25 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-K, M = map(int, input().split())
+K, N = map(int, input().split())
 lengths = list(map(int, input().split()))
 start = 0
 end = max(lengths)
+count = 0
+max_length = 0
 
 while start <= end:
     mid = (start + end)//2
-    for tree in lengths:
-        while tree >0:
-            if tree-mid>0:
-                result+= 1
+    count = 0
+        if tree - mid < 0:
+            continue
+        while tree-mid > 0:
             tree = tree-mid
-    if result==N:
-        print(mid)
-        sys.exit()
-    elif result > N:
-        if max_length < mid:
-            max_length= mid
-        start = mid + 1
-    else:
+            count+= 1
+    if N == count:
+        if mid > max_length:
+            max_length = mid
+    elif N < count:
         end = mid -1
-print(max_length)S
+    else:
+        if max_length < mid:
+            max_length = mid
+        start = mid + 1
+print(max_length)
