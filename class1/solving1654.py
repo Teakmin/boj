ac6a07af91332af9cb06b933aff5ea900de09c31
Teakmin <tmin4889@naver.com>
2021-07-14 21:39:40 +0900

    이분탐색

--- class1/solving1654.py
+++ class1/solving1654.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 
 K, N = map(int, input().split())
 lengths = list(map(int, input().split()))
-start = 0
+start = 1
 end = max(lengths)
 count = 0
 max_length = 0
@@ -11,18 +11,18 @@ max_length = 0
 while start <= end:
     mid = (start + end)//2
     count = 0
-        if tree - mid < 0:
-            continue
-        while tree-mid > 0:
-            tree = tree-mid
-            count+= 1
-    if N == count:
-        if mid > max_length:
-            max_length = mid
-    elif N < count:
-        end = mid -1
-    else:
-        if max_length < mid:
-            max_length = mid
-        start = mid + 1
+    for x in lengths:
+        sum += x//mid
+    if sum < N:
+        right = mid -1
+    elif sum >= mid + 1
 print(max_length)
+
+l   m   r
+199 2-- 400
+199 2-- 3--
+..
+..
+.
+199 200 200
+200
