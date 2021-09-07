6792891f47b37a26bc723a18f3e58d9ef8dab99b
Teakmin <tmin4889@naver.com>
2021-09-07 21:01:06 +0900

    miniheap

---
+++ class1/ｍｉｎｉｈｅａｐ1927.py
@@ -0,0 +1,20 @@
+N = int(input())
+arr = [[-1] for _ in range(int(N/2) + 1)]
+
+# def minheap():
+#     for i in range(1, (N/2) + 1):
+#         if arr[2n] > arr[2n + 1]:
+#             arr[2n], arr[2n + 1] = arr[2n + 1], arr[2n]
+#     if arr[2n] < arr[]
+
+for i in range(N):
+    a = int(input())
+    if a == 0:
+        print(arr.pop[1])
+
+    else:
+        new_number = arr.ｆｉｎｄ(＇－１＇,２)
+        arr.insert(a, new_number)
+
+
+print(arr)
